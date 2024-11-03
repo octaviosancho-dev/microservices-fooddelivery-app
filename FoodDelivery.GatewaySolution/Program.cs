34b0444c6d91e04c61aa8f9866cd03521d53557d
octaviosancho-dev <octaviosanchodev@gmail.com>
2024-11-02 22:27:11 -0300

    fix

--- FoodDelivery.GatewaySolution/Program.cs
+++ FoodDelivery.GatewaySolution/Program.cs
@@ -4,8 +4,6 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
 builder.AddAppAuthentication();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
