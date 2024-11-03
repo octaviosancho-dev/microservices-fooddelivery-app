38bf3ba412b6ab2ad30865714308d57ae6f0ca22
octaviosancho-dev <octaviosanchodev@gmail.com>
2024-11-03 20:48:46 -0300

    Fix

--- FoodDelivery.GatewaySolution/Program.cs
+++ FoodDelivery.GatewaySolution/Program.cs
@@ -5,7 +5,9 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAppAuthentication();
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
